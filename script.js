4c63f8174ed683ef585de69c63f73b45e8c8d143
sumitsingh34398-art <sumitsingh34398@gmail.com>
2026-06-12 13:07:10 +0530

    Add login form submission handler

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+const form = document.querySelector("form");
 
+form.addEventListener("submit", function(event){
+    event.preventDefault();
+
+    alert("Login Successful! Welcome to CloudVault Pro");
+
+    console.log("User Logged In");
+});
